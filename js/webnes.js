cfd162082a5e17779b90f9ae86ccbc7abe93bdec
admin <zodttd@ME.local>
2016-05-06 23:08:15 -0400

    5
    5

--- js/webnes.js
+++ js/webnes.js
@@ -80,12 +80,24 @@ WebNES.prototype = {
       var source = this.audio.createBufferSource();
 
       if(this.audiobuffer == null) {
-        this.audiobuffer = this.audio.createBuffer(2, leftSamples.length, this.nes.papu.sampleRate);
+        this.audiobuffer = this.audio.createBuffer(2, 44100, 44100);
       }
+
+  for (var channel = 0; channel < 2; channel++) {
+    // This gives us the actual array that contains the data
+    var nowBuffering = this.audiobuffer.getChannelData(channel);
+    for (var i = 0; i < 44100; i++) {
+      // Math.random() is in [0; 1.0]
+      // audio needs to be in [-1.0; 1.0]
+      nowBuffering[i] = Math.random() * 2 - 1;
+    }
+  }
+
       //var buffer = this.audio.createBuffer(2, leftSamples.length, this.nes.papu.sampleRate);
-      this.audiobuffer.getChannelData(0).set(leftSamples);
-      this.audiobuffer.getChannelData(1).set(rightSamples);
-      source.buffer = this.audiobuffer;
+      
+      //this.audiobuffer.getChannelData(0).set(leftSamples);
+      //this.audiobuffer.getChannelData(1).set(rightSamples);
+      source.buffer = nowBuffering;//this.audiobuffer;
       source.connect(this.audio.destination);
       if (source.start) {
           source.start(0);
